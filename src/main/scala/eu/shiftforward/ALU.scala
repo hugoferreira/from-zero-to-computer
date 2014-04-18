3f935123ca29441866e2c4dfab31c562fc692999
Hugo Ferreira <hugo@shiftforward.eu>
2014-04-18 22:45:57 +0100

    More ALU magic

--- src/main/scala/eu/shiftforward/ALU.scala
+++ src/main/scala/eu/shiftforward/ALU.scala
@@ -4,7 +4,6 @@ class ALU extends ArithmeticElements with ControlFlowElements {
   def alu(x: Bus, y: Bus, zx: Wire, nx: Wire, zy: Wire, ny: Wire, f: Wire, no: Wire): (Bus, Wire, Wire, Wire) = {
     assert(x.size == y.size)
 
-    val ng, zr = new Wire
     val zeros = constant(x.size)(0x00)
 
     val x1 = mux(x, zeros, zx)
@@ -14,9 +13,8 @@ class ALU extends ArithmeticElements with ControlFlowElements {
     val y2 = mux(y1, inverter(y1), ny)
 
     val (sum, of) = multiBitAdder(x2, y2)
-    val ands = and(x2, y2)
-    val out = mux(sum, ands, f)
+    val out = mux(and(x2, y2), sum, f)
 
-    (mux(out, zeros, no), ng, zr, mux(of, Ground, f))
+    (mux(out, inverter(out), no), Ground, Ground, mux(Ground, of, f))
   }
 }
